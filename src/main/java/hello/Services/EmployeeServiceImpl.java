045f2dc2968ccd119570667d1b343d30b8fc980f
CsatoMate <CsatoMate@users.noreply.github.com>
2018-07-25 13:40:27 +0200

    Create database operations

--- src/main/java/hello/Services/EmployeeServiceImpl.java
+++ src/main/java/hello/Services/EmployeeServiceImpl.java
@@ -4,8 +4,10 @@ import hello.Models.Employee;
 import hello.Repositorys.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl{
@@ -16,5 +18,33 @@ public class EmployeeServiceImpl{
     public List<Employee> findAll() {
         return employeeRepository.findAll();
     }
+
+    public Optional<Employee> findById(Long pId) {
+        return employeeRepository.findById(pId);
+    }
+
+    public void addEmployee(Employee pEmployee){
+        employeeRepository.save(pEmployee);
+    }
+
+    public void deleteEmployeeByID(Long pId){
+        employeeRepository.deleteById(pId);
+    }
+
+    public void deleteEmployeeByName(Employee pEmployee){
+        employeeRepository.delete(pEmployee);
+    }
+
+    public void deleteAllEmployees(){
+        employeeRepository.deleteAll();
+    }
+
+    public void saveEmployee(Employee pEmployee){
+        employeeRepository.save(pEmployee);
+    }
+
+
+
+
 
 }
